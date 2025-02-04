369c35f9c5af3eb428f89492d83cb8f5424a4066
Brun0 <brunodev41@gmail.com>
2025-02-04 16:54:44 -0300

    Adição dos Scafolds e inicio do banco de dados

---
+++ ListaTarefaApi/Data/ListaTarefasContext.cs
@@ -0,0 +1,16 @@
+﻿using ListaTarefaApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ListaTarefaApi.Data
+{
+    public class ListaTarefasContext : DbContext
+    {
+        public ListaTarefasContext(DbContextOptions<ListaTarefasContext> options) : base(options)
+        {
+        }
+        public DbSet<Tarefa> Tarefas { get; set; }
+        public DbSet<TipoTarefa> TipoTarefa { get; set; }
+    }
+}
+
