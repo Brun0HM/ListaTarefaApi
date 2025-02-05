4aca41e3915546c262f4627ec542b0a52af8a37a
Brun0 <brunodev41@gmail.com>
2025-02-05 09:22:48 -0300

    Inclusão Do metodo finalizar tarefa

--- ListaTarefaApi/Controllers/TarefasController.cs
+++ ListaTarefaApi/Controllers/TarefasController.cs
@@ -42,6 +42,20 @@ namespace ListaTarefaApi.Controllers
             return tarefa;
         }
 
+        [HttpGet("concluidas")]
+        //A public sempre é async pois caso contrario travaria o sistema
+        //Ações get sempre são do tipo get
+        public async Task<ActionResult<IEnumerable<Tarefa>>> GetTarefasConcluidas()
+        {
+            // =>-Tal que 
+            //Retorna as tarefas que estão concluidas
+            return await _context.Tarefas.Where(t => t.Concluida == true).ToListAsync();
+        }
+
+
+
+
+
         // PUT: api/Tarefas/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
@@ -103,6 +117,36 @@ namespace ListaTarefaApi.Controllers
         private bool TarefaExists(int id)
         {
             return _context.Tarefas.Any(e => e.tarefaId == id);
+        }
+        [HttpPut("finalizarTarefa/{id}")]
+        public async Task<IActionResult> FinalizarTarefa(int id)
+        {
+            var tarefa = await _context.Tarefas.FindAsync(id);
+            if (tarefa == null)
+            {
+                return NotFound();
+            }
+
+            tarefa.Concluida = true;
+            _context.Entry(tarefa).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TarefaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
         }
     }
 }
